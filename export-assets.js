c4b6ef9feeeda51fa9d4d2d6a177895cb3c30a10
Bilaal Yadallee <bilaal.yadallee@insign.fr>
2022-05-30 23:48:28 +0200

    [SCRIPT] Fix path of data directory

--- export-assets.js
+++ export-assets.js
@@ -1,6 +1,7 @@
 const fs = require('fs-extra')
+const path = require('path')
 const { argv } = require('process')
-const sourceDirectory = 'data'
+const sourceDirectory = path(__dirname,'data')
 const destDirectory = argv[2]
 
 if(fs.existsSync(destDirectory))
